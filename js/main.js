b395057b1b3dcdeb7d9c46164fd58c66cdaee1c2
gael <kledii@hotmail.com>
2016-02-27 11:44:50 +0100

    starting transformation before GDC

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+function resizeTitle()
+{   
+    var docHeight = $(window).height();
+    var titleHeight = $("#title").height();
+    var padding = (docHeight * 0.5) - (titleHeight * 0.5);
+    $("#title-banner").css({"padding-top":padding, "padding-bottom":padding});  
+}
+$(window).ready(function(){
+    resizeTitle();
+    $("#container").css({"visibility":"visible"});   
+});
+$( window ).resize(resizeTitle);
